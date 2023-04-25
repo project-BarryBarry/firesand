550ce132dbb7df511df14622c8b13b4037579a56
晄俠金田 <arikyosan@outlook.jp>
2023-04-25 15:30:08 +0900

    run as user

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 # Run the application background
 sudo -u ubuntu nohup /opt/wizvera/veraport/veraport &
 
+sudo -u ubuntu nohup /opt/INITECH/crosswebex/INISAFECrossWebEXSvc &
+
 touch /dev/mem
 
 nohup /opt/AhnLab/ASTx/astxdaemon &
-
-nohup /opt/INITECH/crosswebex/INISAFECrossWebEXSvc &
 
 # Run haproxy
 nohup haproxy -f /etc/haproxy/haproxy.cfg &
