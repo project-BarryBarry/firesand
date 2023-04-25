167177d44eff5cce708012cac2a21498d991f119
晄俠金田 <arikyosan@outlook.jp>
2023-04-25 15:11:30 +0900

    fix typo

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # Run the application background
-sudo -u ubutu nohup /opt/wizvera/veraport/veraport &
+sudo -u ubuntu nohup /opt/wizvera/veraport/veraport &
 
 touch /dev/mem
 
 nohup /opt/AhnLab/ASTx/astxdaemon &
+
+nohup /opt/INITECH/crosswebex/INISAFECrossWebEXSvc &
 
 # Run haproxy
 nohup haproxy -f /etc/haproxy/haproxy.cfg &
